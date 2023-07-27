90725fb247e738a0cec9cf7a9135cbc92564f5af
weijiang2023 <124419632+weijiang2023@users.noreply.github.com>
2023-07-27 23:53:56 +0800

    Update config.py

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ available_setting = {
     "group_chat_reply_suffix": "",  # 群聊时自动回复的后缀，\n 可以换行
     "group_chat_keyword": [],  # 群聊时包含该关键词则会触发机器人回复
     "group_at_off": False,  # 是否关闭群聊时@bot的触发
-    "group_name_white_list": ["ChatGPT测试群", "ChatGPT测试群2"],  # 开启自动回复的群名称列表
+    "group_name_white_list": ["ChatGPT测试群", "ChatGPT测试群2", "星映i熊猫与AI原优舍", "f"],  # 开启自动回复的群名称列表
     "group_name_keyword_white_list": [],  # 开启自动回复的群名称关键词列表
     "group_chat_in_one_session": ["ChatGPT测试群"],  # 支持会话上下文共享的群名称
     "trigger_by_self": False,  # 是否允许机器人触发
