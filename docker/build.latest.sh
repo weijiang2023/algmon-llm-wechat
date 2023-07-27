283c427eb0063ac880e2772df88dba81bc353e3d
weijiang2023 <124419632+weijiang2023@users.noreply.github.com>
2023-07-28 00:31:16 +0800

    Update build.latest.sh

--- docker/build.latest.sh
+++ docker/build.latest.sh
@@ -3,6 +3,6 @@
 unset KUBECONFIG
 
 cd .. && docker build -f docker/Dockerfile.latest \
-             -t zhayujie/chatgpt-on-wechat .
+             -t weijiang2009/algmon-llm-wechat .
 
 docker tag zhayujie/chatgpt-on-wechat zhayujie/chatgpt-on-wechat:$(date +%y%m%d)
