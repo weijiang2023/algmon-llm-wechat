bd2edeebd9669f542255863e1cdc314990e75ba7
江纬 <wei@algmon.com>
2023-07-28 00:42:29 +0800

    UPDATE build.latest.sh

--- docker/build.latest.sh
+++ docker/build.latest.sh
@@ -5,4 +5,4 @@ unset KUBECONFIG
 cd .. && docker build -f docker/Dockerfile.latest \
              -t weijiang2009/algmon-llm-wechat .
 
-docker tag zhayujie/chatgpt-on-wechat zhayujie/chatgpt-on-wechat:$(date +%y%m%d)
+docker tag weijiang2009/algmon-llm-wechat weijiang2009/algmon-llm-wechat:$(date +%y%m%d)
